6f1a25b547885f9c87dbcef558ab8ec8d83c46c8
stephorpilla <stephanieorpilla@yahoo.com>
2016-09-24 11:51:50 -0700

    moved add.js into makeRes.html

--- add.js
+++ add.js
@@ -1,3 +1,22 @@
+// $('#addBtn').on('click', function () {
+// 	var newCharacter = {
+// 		name: $('#name').val().trim(),
+// 		role: $('#role').val().trim(),
+// 		age: $('#age').val().trim(),
+// 		forcePoints: $('#forcepoints').val().trim()
+// 	};
+
+// 	var currentURL = window.location.origin;
+
+// 	// Question: What does this code do??
+// 	$.post(currentURL + '/api/new', newCharacter)
+// 		.done(function (data) {
+// 			console.log(data);
+// 			alert('Adding character...');
+// 		});
+
+// 	return false;
+// });
 $("#submitIn").on("click",function(){
 	var newReservation = {
 		name: $("#nameIn").val().trim(),
@@ -5,12 +24,16 @@ $("#submitIn").on("click",function(){
 		email: $("#emailIn").val().trim(),
 		uniqueID: $("#idIn").val().trim()
 	};
-	console.log("Did it.");
+	
+	var currentURL = window.location.origin;
 
-	$.post('http://localhost:3000/api/new', newReservation).done(function (data) {
-		console.log(data);
-		alert('Adding reservation...');
-	});
+	// Question: What does this code do??
+	$.post(currentURL + '/api/new', newReservation)
+		.done(function (data) {
+			console.log(data);
+			alert('Adding reservation...');
+		});
 
 	return false;
 });
+
